98d06ffc5c069358fe1d5e142537b1dae0b9886f
niki914 <niki27687@gmail.com>
2026-06-09 01:24:00 +0800

    release: 2-0.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.niki914.nexus.agentic"
         minSdk = 26
         targetSdk = 34
-        versionName = "0.0.1"
-        versionCode = 1
+        versionName = "0.0.2"
+        versionCode = 2
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
